88d50f81c7b0cd261a820a3ef491cd99fa8addbb
GurnMi <dlrjsak3@naver.com>
2017-10-11 14:18:37 +0900

    dto 생성완료

---
+++ src/kr/or/dgit/jdbc_application/dto/Title.java
@@ -0,0 +1,37 @@
+package kr.or.dgit.jdbc_application.dto;
+
+public class Title {
+	private int titleNo;
+	private String titlename;
+
+	public Title() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public int getTitleNo() {
+		return titleNo;
+	}
+
+	public void setTitleNo(int titleNo) {
+		this.titleNo = titleNo;
+	}
+
+	public String getTitlename() {
+		return titlename;
+	}
+
+	public void setTitlename(String titlename) {
+		this.titlename = titlename;
+	}
+
+	public Title(int titleNo, String titlename) {
+		this.titleNo = titleNo;
+		this.titlename = titlename;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("title [titleNo=%s, titlename=%s]", titleNo, titlename);
+	}
+
+}
